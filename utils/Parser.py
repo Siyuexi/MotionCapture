40a65aaeba3eb5446992d9268b1538d3ced8b124
Siyuexi <798034039@qq.com>
2022-04-20 20:15:59 +0800

    --Updated=Changed the logic of data reading

--- utils/Parser.py
+++ utils/Parser.py
@@ -12,31 +12,54 @@ class Parser(data.Dataset):
         self.annot_path = annot_path
         self.img_path = img_path
         self.img_size = img_size
+        
+        self.last_name = self.annot[0]['image'] # some people are in the same image, and these samples are adjacent to each other
+        self.index_shift = 0
 
         file = open(annot_path, "rb")
         self.annot = json.load(file) 
         file.close()
 
     def __getitem__(self,index):
-        img_name = self.annot[index]['image'] # a string
+        idx = index + self.index_shift
+        img_name = self.annot[idx]['image'] # a string
 
-        img = Image.open(self.img_path+'/'+img_name)
-        w = img.width
-        h = img.height
-        img.resize((self.img_size,self.img_size))
-        img = np.array(img)
+        joints = []
+        bboxes = []
 
-        joints = np.array(self.annot[index]['joints'])
-        joints = np.array([joints[:,0]/w,joints[:,1]/h]).transpose()
-        joints = joints*self.img_size
-        
-        hl = self.annot[index]['scale']*100
-        cx = self.annot[index]['center'][0]
-        cy = self.annot[index]['center'][1]
-        bbox = np.array([(cx-hl)/w,(cy-hl)/h,(cx+hl)/w,(cy+hl)/h])
-        bbox = bbox * self.img_size
+        while(img_name==self.last_name):
+            self.last_name = img_name
+            self.index_shift += 1 # add shift
 
-        return img,joints,bbox
+            img = Image.open(self.img_path+'/'+img_name)
+            w = img.width
+            h = img.height
+            img.resize((self.img_size,self.img_size))
+            img = np.array(img)
+
+            joint = np.array(self.annot[idx]['joints'])
+            joint = np.array([joint[:,0]/w,joint[:,1]/h]).transpose()
+            joint = joint*self.img_size
+            
+            hl = self.annot[idx]['scale']*100
+            cx = self.annot[idx]['center'][0]
+            cy = self.annot[idx]['center'][1]
+            bbox = np.array([(cx-hl)/w,(cy-hl)/h,(cx+hl)/w,(cy+hl)/h])
+            bbox = bbox * self.img_size
+
+            joints.append(joint)
+            bboxes.append(bbox)
+
+        return img,joints,bboxes
     
     def __len__(self):
-        return len(self.annot)
+        cnt = 0
+        ln = ''
+        le = len(self.annot)
+        for i in range(le):
+            cn = self.annot[i]['image']
+            if(ln==cn):
+                cnt += 1
+            ln = cn
+        le = le - cn
+        return le
